f3328933dab12cd6b706da6760260c7f7dd9f217
OsMax <www.osmax@gmail.com>
2023-06-30 10:55:23 +0300

    Task_2

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,6 +1,29 @@
 import './css/statistics.css';
 import Prop from 'prop-types';
 
+function getRandomHexColor() {
+  //   return `#${Math.floor(Math.random() * 16777215)
+  //     .toString(16)
+  //     .padStart(6, 0)}`;
+  const color = Math.floor(Math.random() * (200 - 150) + 150)
+    .toString(16)
+    .padStart(2, 0);
+  return (
+    '#' +
+    Math.floor(Math.random() * (250 - 150) + 150)
+      .toString(16)
+      .padStart(2, 0) +
+    '' +
+    Math.floor(Math.random() * (250 - 150) + 150)
+      .toString(16)
+      .padStart(2, 0) +
+    '' +
+    Math.floor(Math.random() * (250 - 150) + 150)
+      .toString(16)
+      .padStart(2, 0)
+  );
+}
+
 export const Statistics = ({ title, stats }) => {
   return (
     <section className="statistics">
@@ -8,9 +31,13 @@ export const Statistics = ({ title, stats }) => {
 
       <ul className="stat-list">
         {stats.map(stat => (
-          <li key={stat.id} className="item">
+          <li
+            key={stat.id}
+            className="item"
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
             <span className="label">{stat.label}</span>
-            <span className="percentage">{stat.percentage}</span>
+            <span className="percentage">{stat.percentage + '%'}</span>
           </li>
         ))}
       </ul>
