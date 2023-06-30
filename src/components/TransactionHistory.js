264b88bb77c6054f5ccf631d0fd2ebbc739df8fc
OsMax <www.osmax@gmail.com>
2023-06-30 13:22:17 +0300

    Task_4

---
+++ src/components/TransactionHistory.js
@@ -0,0 +1,25 @@
+import './css/transactions.css';
+
+export const TransactionHistory = ({ transactions }) => {
+  return (
+    <table class="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(transaction => (
+          <tr key={transaction.id}>
+            <td>{transaction.type}</td>
+            <td>{transaction.amount}</td>
+            <td>{transaction.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
